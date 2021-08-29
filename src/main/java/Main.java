f8259da67e30dfaee3242dd7fce8b4fe8e04d434
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-08-30 00:44:30 +0300

    Initial commit автоматический создаем таблицу и добавляем туда элементы

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import ru.eshmakar.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Student("Max"));
+        session.getTransaction().commit();
+        session.close();
+    }
+}
